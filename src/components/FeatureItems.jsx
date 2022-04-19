791e41e6082b9143a5a09984466434425edaa21f
mazylol <landonp95@gmail.com>
2022-04-18 20:51:26 -0400

    feature list

--- src/components/FeatureItems.jsx
+++ src/components/FeatureItems.jsx
@@ -1,7 +1,7 @@
 function FeatureItems() {
   return (
     <>
-      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center">
+      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center duration-150">
         <p className="text-xl sm:text-2xl text-gray-900 inline-block">
           Frameworks
         </p>
@@ -9,7 +9,7 @@ function FeatureItems() {
           Provides gives general information on web frameworks
         </p>
       </div>
-      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center">
+      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center duration-150">
         <p className="text-xl sm:text-2xl text-gray-900 inline-block">
           Languages
         </p>
@@ -17,12 +17,36 @@ function FeatureItems() {
           Provides top notch information on programming languages
         </p>
       </div>
-      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center">
+      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center duration-150">
         <p className="text-xl sm:text-2xl text-gray-900 inline-block">
           People
         </p>
         <p className="text-sm sm:text-md text-gray-800 pt-2 w-auto">
           Information about the people who contributed to the field of programming
+        </p>
+      </div>
+      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center duration-150">
+        <p className="text-xl sm:text-2xl text-gray-900 inline-block">
+          Tools (Soon)
+        </p>
+        <p className="text-sm sm:text-md text-gray-800 pt-2 w-auto">
+          Tools that help out programmers every day
+        </p>
+      </div>
+      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center duration-150">
+        <p className="text-xl sm:text-2xl text-gray-900 inline-block">
+          Version Control (Soon)
+        </p>
+        <p className="text-sm sm:text-md text-gray-800 pt-2 w-auto">
+          Information on systems like git and github
+        </p>
+      </div>
+      <div className="max-w-3xl flex flex-col justify-center mx-auto bg-white shadow-xl hover:shadow-2xl rounded-xl p-5 m-5 text-center duration-150">
+        <p className="text-xl sm:text-2xl text-gray-900 inline-block">
+          Libraries (Soon)
+        </p>
+        <p className="text-sm sm:text-md text-gray-800 pt-2 w-auto">
+          Provides gives general information on libraries
         </p>
       </div>
     </>
--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -5,7 +5,7 @@ function Invite() {
   return (
     <div className="max-w-lg w-full text-center m-auto">
       <a href="https://github.com/discode-devs/discode" target="_blank" className="px-5">
-        <span className="mt-6 inline-block w-auto bg-black font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer rounded-xl hover:animate-wiggle">
+        <span className="mt-6 inline-block w-auto bg-black font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer rounded-xl">
           <div className="flex flex-row items-center">
             <span className="mr-3">Source</span>
             <BsCodeSlash color="white" />
@@ -13,7 +13,7 @@ function Invite() {
         </span>
       </a>
       <a href="https://discord.com/api/oauth2/authorize?client_id=937476981321240626&permissions=139586825280&scope=bot%20applications.commands" target="_blank" className="px-5">
-        <span className="mt-6 inline-block w-auto bg-black font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer rounded-xl hover:animate-wiggle">
+        <span className="mt-6 inline-block w-auto bg-black font-display text-white text-base md:text-xl py-4 px-7 cursor-pointer rounded-xl">
           <div className="flex flex-row items-center">
             <span className="mr-3">Invite</span>
             <FaPaperPlane color="white" />
